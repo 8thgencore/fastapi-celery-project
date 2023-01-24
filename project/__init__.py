61c0bbfff333069401025d5c2b9f08e987415e0c
8thgencore <axelmaker@ya.ru>
2023-01-24 20:52:53 +0000

    ✨ Add Test-Driven Development

--- project/__init__.py
+++ project/__init__.py
@@ -10,18 +10,27 @@ def create_app() -> FastAPI:
     app = FastAPI()
 
     from project.logging import configure_logging
+
     configure_logging()
 
     from project.celery_utils import create_celery
+
     app.celery_app = create_celery()
 
     from project.users import users_router
+
     app.include_router(users_router)
 
+    from project.tdd import tdd_router
+
+    app.include_router(tdd_router)
+
     from project.ws import ws_router
+
     app.include_router(ws_router)
 
     from project.ws.views import register_socketio_app
+
     register_socketio_app(app)
 
     @app.on_event("startup")
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,12 @@ import pytest
 
 os.environ["FASTAPI_CONFIG"] = "testing"  # noqa
 
-from project.users.factories import UserFactory
 from pytest_factoryboy import register
+from project.users.factories import UserFactory
+from project.tdd.factories import MemberFactory
 
 register(UserFactory)
+register(MemberFactory)
 
 
 @pytest.fixture
@@ -41,3 +43,8 @@ def client(app):
     from fastapi.testclient import TestClient
 
     yield TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def tmp_upload_dir(tmpdir, settings):
+    settings.UPLOADS_DEFAULT_DEST = tmpdir.mkdir("tmp")
